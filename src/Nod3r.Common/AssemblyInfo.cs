883b5c3ac8a8d63bcf5e5b73755f7f79d4afa610
Roudenn <romabond091@gmail.com>
2026-08-29 10:30:51 +0300

    Add even more projects

---
+++ src/Nod3r.Common/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Nod3r.Solver")]
+[assembly: InternalsVisibleTo("Nod3r.Solver.Tests")]
+[assembly: InternalsVisibleTo("Nod3r.Solver.Benchmarks")]
---
+++ src/Nod3r.Solver/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Nod3r.API")]
+[assembly: InternalsVisibleTo("Nod3r.API.Tests")]
+[assembly: InternalsVisibleTo("Nod3r.Solver.Tests")]
+[assembly: InternalsVisibleTo("Nod3r.Solver.Benchmarks")]
